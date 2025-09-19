54285e4c55cacb43c191bd47b4cd4e9bcfc59b54
Giperbola <egor.crut2017@yandex.ru>
2025-09-20 00:06:09 +0300

    Cmake

--- GiperbolaBook/Main.cpp
+++ GiperbolaBook/Main.cpp
@@ -1,62 +1,62 @@
 #include "include/GiperbolaBookApp.hpp"
 
 
-//int main(int argc, char* argv[])
-//{
-//    std::string ip_server = "127.0.0.1";
-//    uint16_t port_server = 8888;
-//
-//    if (argc >= 2) {
-//        ip_server = argv[1];
-//    }
-//
-//    if (argc >= 3) {
-//        try {
-//            port_server = static_cast<uint16_t>(std::stoi(argv[2]));
-//        }
-//        catch (...) {
-//            std::cerr << "Invalid server port, using default: " << port_server << std::endl;
-//        }
-//    }
-//
-//    GiperbolaBookApp app(ip_server, port_server);
-//    app.run();
-//
-//    return 0;
-//}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int main(int argc, char* argv[])
 {
-    int argc = 0;
-    LPWSTR* argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+   std::string ip_server = "127.0.0.1";
+   uint16_t port_server = 8888;
 
-    std::vector<std::string> argv(argc);
-    for (int i = 0; i < argc; i++) {
-        int size = WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, nullptr, 0, nullptr, nullptr);
-        std::string arg(size - 1, '\0');
-        WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, arg.data(), size, nullptr, nullptr);
-        argv[i] = std::move(arg);
-    }
-    LocalFree(argvW);
+   if (argc >= 2) {
+       ip_server = argv[1];
+   }
 
-    std::string ip_server = "127.0.0.1";
-    uint16_t port_server = 8888;
+   if (argc >= 3) {
+       try {
+           port_server = static_cast<uint16_t>(std::stoi(argv[2]));
+       }
+       catch (...) {
+           std::cerr << "Invalid server port, using default: " << port_server << std::endl;
+       }
+   }
 
-    if (argc >= 2) {
-        ip_server = argv[1];
-    }
+   GiperbolaBookApp app(ip_server, port_server);
+   app.run();
 
-    if (argc >= 3) {
-        try {
-            port_server = static_cast<uint16_t>(std::stoi(argv[2]));
-        }
-        catch (...) {
-            MessageBoxA(nullptr, "Invalid server port, using default", "Warning", MB_OK | MB_ICONWARNING);
-        }
-    }
-
-    GiperbolaBookApp app(ip_server, port_server);
-    app.run();
-
-    return 0;
+   return 0;
 }
+
+// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+// {
+//     int argc = 0;
+//     LPWSTR* argvW = CommandLineToArgvW(GetCommandLineW(), &argc);
+
+//     std::vector<std::string> argv(argc);
+//     for (int i = 0; i < argc; i++) {
+//         int size = WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, nullptr, 0, nullptr, nullptr);
+//         std::string arg(size - 1, '\0');
+//         WideCharToMultiByte(CP_UTF8, 0, argvW[i], -1, arg.data(), size, nullptr, nullptr);
+//         argv[i] = std::move(arg);
+//     }
+//     LocalFree(argvW);
+
+//     std::string ip_server = "127.0.0.1";
+//     uint16_t port_server = 8888;
+
+//     if (argc >= 2) {
+//         ip_server = argv[1];
+//     }
+
+//     if (argc >= 3) {
+//         try {
+//             port_server = static_cast<uint16_t>(std::stoi(argv[2]));
+//         }
+//         catch (...) {
+//             MessageBoxA(nullptr, "Invalid server port, using default", "Warning", MB_OK | MB_ICONWARNING);
+//         }
+//     }
+
+//     GiperbolaBookApp app(ip_server, port_server);
+//     app.run();
+
+//     return 0;
+// }
